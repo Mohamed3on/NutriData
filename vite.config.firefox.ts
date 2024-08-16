9161ae97ebea4e9fb4ff5530e50f545ec8ecfe39
ReecerTV <r3769.tv@gmail.com>
2024-08-16 19:02:34 +0200

    Removes redundancies

--- vite.config.firefox.ts
+++ vite.config.firefox.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite';
 import { crx } from '@crxjs/vite-plugin';
-import fs from 'fs';
-import path from 'path';
-
-const manifestPath = path.resolve(__dirname, 'firefox/manifest.json');
-const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
-
-// Remove unwanted properties
-delete manifest.use_dynamic_url;
+import manifest from './firefox/manifest.json';
 
 export default defineConfig({
   build: {
